b8f3ec704edd74517ac9b320fb45a13c15e32aef
GitUserShreyash <ambureshreyash7@gmail.com>
2026-08-11 23:59:29 +0530

    Removed unnecessary files

--- src/components/profile/LifestyleCard.jsx
+++
@@ -1,9 +0,0 @@
-function LifestyleCard() {
-    return (
-        <div className="bg-white rounded-2xl border border-gray-200 p-6">
-            Lifestyle
-        </div>
-    );
-}
-
-export default LifestyleCard;
